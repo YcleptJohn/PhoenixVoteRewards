1213c66643ea366df266251cc0643e5001a86e55
YcleptJohn <YcleptJohn@gmail.com>
2015-05-04 23:21:57 +0100

    -test output; +basic format preparation(onEnable/onDisable/onCommand)

--- PhoenixVoteRewards/src/uk/co/ycleptjohn/phoenixvotes/PhoenixVoteRewards.java
+++ PhoenixVoteRewards/src/uk/co/ycleptjohn/phoenixvotes/PhoenixVoteRewards.java
@@ -1,5 +1,19 @@
 package uk.co.ycleptjohn.phoenixvotes;
 
-public class PhoenixVoteRewards {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PhoenixVoteRewards extends JavaPlugin {
+	public void onEnable() {
+		
+	}
+	public void onDisable() {
+		
+	}
 	
+	public boolean onCommand(CommandSender cmdSender, Command cmd, String label, String[] args) {
+			
+		return false;
+	}
 }
