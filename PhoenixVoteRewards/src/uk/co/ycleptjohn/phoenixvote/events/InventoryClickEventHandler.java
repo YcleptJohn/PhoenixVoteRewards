46f120d8328cdcbd907289ef8bb1a25580b35009
John <YcleptJohn@gmail.com>
2016-01-05 23:06:01 +0000

    +closer towards final structure+process rather than test code

--- PhoenixVoteRewards/src/uk/co/ycleptjohn/phoenixvote/events/InventoryClickEventHandler.java
+++ PhoenixVoteRewards/src/uk/co/ycleptjohn/phoenixvote/events/InventoryClickEventHandler.java
@@ -1,54 +1,58 @@
 package uk.co.ycleptjohn.phoenixvote.events;
 
-import java.util.List;
-
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 
 import uk.co.ycleptjohn.phoenixvote.Perk;
+import uk.co.ycleptjohn.phoenixvote.PhoenixVote;
 
 public class InventoryClickEventHandler implements Listener {
+	Plugin plugin = PhoenixVote.getPlugin();
+	
 	@EventHandler
 	public void onInventoryClickEvent(InventoryClickEvent e) {
 		//Player p = (Player) e.getWhoClicked();
-		ItemStack clickedperk = e.getCurrentItem();
-		
-		List<String> clickedLore = clickedperk.getItemMeta().getLore();
-		String clickedLoreHiddenLine = clickedLore.get(1);
-		Bukkit.broadcastMessage("Hidden:" + clickedLoreHiddenLine);
-		Bukkit.broadcastMessage("Unhidden:" + convertToVisibleString(clickedLoreHiddenLine));
-		String clickedPerkKey = convertToVisibleString(clickedLoreHiddenLine);
-		Perk perk = new Perk(clickedPerkKey);
-		Bukkit.broadcastMessage(perk.getNameColours());
-		Bukkit.broadcastMessage(perk.getName());
-		Bukkit.broadcastMessage(perk.getCost() + "");
-		Bukkit.broadcastMessage(perk.getDescription().toString());
-		Bukkit.broadcastMessage(perk.getMaterial().name());
+		if (ChatColor.stripColor(e.getInventory().getTitle()).contains("PhoenixCraft") && (e.getCurrentItem().getType() != Material.AIR)) {
+			e.setCancelled(true);
+			ItemStack clickedPerkItem = e.getCurrentItem();
+			handleInitialClick(clickedPerkItem);
+		}
 	}
 	
-	public void confirmPurchase() {
+	public void handleInitialClick(ItemStack clickedPerkItem) {
+		if (clickedPerkItem.getItemMeta().getLore().get(1) != null) {
+			String clickedConfigKey = convertToVisibleString(clickedPerkItem.getItemMeta().getLore().get(1)); // Pull hidden config key from lore and make it visible
+			if (plugin.getConfig().contains("store.perks." + clickedConfigKey)) {
+				Perk clickedPerk = new Perk(clickedConfigKey);
+				confirmPurchase(clickedPerk);
+			}
+		}
+	}
+
+	public void confirmPurchase(Perk chosenPerk) {
 		
 	}
-	
+
 	public void handlePurchase() {
-		
+
 	}
-	
+
 	public static String convertToInvisibleString(String s) {
 		String hidden = "";
 		for (char c : s.toCharArray()) hidden += ChatColor.COLOR_CHAR+""+c;
 		return hidden;
 	}
-	
+
 	public static String convertToVisibleString(String s) {
 		String unhidden = s.replaceAll("§", "");
 		return unhidden;
 	}
-	
-	
-	
+
+
+
 }
