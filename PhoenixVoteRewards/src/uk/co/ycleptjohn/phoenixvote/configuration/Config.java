251f66131b4c7e78abfa2d95772d9c8d3f7b896a
John <YcleptJohn@gmail.com>
2016-01-05 23:41:08 +0000

    Structured fields + getter methods; +functional method skeletons

--- PhoenixVoteRewards/src/uk/co/ycleptjohn/phoenixvote/configuration/Config.java
+++ PhoenixVoteRewards/src/uk/co/ycleptjohn/phoenixvote/configuration/Config.java
@@ -1,5 +1,36 @@
 package uk.co.ycleptjohn.phoenixvote.configuration;
 
-public class Config {
+import java.io.File;
 
+public class Config {
+	private String name;
+	private File file;
+	private String defaultResourcePath;
+	
+	public Config(String configName, File configFile, String defaultResourcePath) {
+		name = configName;
+		file = configFile;
+		this.defaultResourcePath = defaultResourcePath;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public File getFile() {
+		return file;
+	}
+	
+	public String getDefaultResourcePath() {
+		return defaultResourcePath;
+	}
+	
+	public void save() {
+		//TODO
+	}
+	
+	public void reset() {
+		//TODO
+	}
+	
 }
