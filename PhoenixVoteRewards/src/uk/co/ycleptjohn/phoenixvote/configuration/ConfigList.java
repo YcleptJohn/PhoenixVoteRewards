587d10a64f4a2664c18cb5f47bf068bc6854e677
John <YcleptJohn@gmail.com>
2016-01-05 23:12:24 +0000

    Initial commit + base code

---
+++ PhoenixVoteRewards/src/uk/co/ycleptjohn/phoenixvote/configuration/ConfigList.java
@@ -0,0 +1,14 @@
+package uk.co.ycleptjohn.phoenixvote.configuration;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+public class ConfigList {
+	private Map<String, List<String>> configs;
+	
+	public ConfigList(String configName, File configFile, File defaultConfigResource) {
+		
+	}
+	
+}
