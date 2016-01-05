19ae83f316f59407d676525c2f846091677f84b6
John <YcleptJohn@gmail.com>
2016-01-05 23:41:44 +0000

    Converted to store Config objects rather than List<String> of details

--- PhoenixVoteRewards/src/uk/co/ycleptjohn/phoenixvote/configuration/ConfigList.java
+++ PhoenixVoteRewards/src/uk/co/ycleptjohn/phoenixvote/configuration/ConfigList.java
@@ -1,14 +1,22 @@
 package uk.co.ycleptjohn.phoenixvote.configuration;
 
 import java.io.File;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ConfigList {
-	private Map<String, List<String>> configs;
+	private static Map<String, Config> configs;
+	//String = config name; Config = config object;
 	
-	public ConfigList(String configName, File configFile, File defaultConfigResource) {
-		
+	public ConfigList() {
+		configs = new HashMap<String, Config>();
 	}
 	
+	public static Map<String, Config> getConfigMap() {
+		return configs;
+	}
+	
+	public void add(String configName, File configFile, String defaultResourcePath) {
+		
+	}
 }
