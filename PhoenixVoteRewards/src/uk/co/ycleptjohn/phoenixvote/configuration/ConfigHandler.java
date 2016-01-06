d604d2758c3d337e496ba266c3946c850ed53fcf
John <YcleptJohn@gmail.com>
2016-01-06 00:11:26 +0000

    +convert getConfig for new Config object storage +saveConfigs() convert

--- PhoenixVoteRewards/src/uk/co/ycleptjohn/phoenixvote/configuration/ConfigHandler.java
+++ PhoenixVoteRewards/src/uk/co/ycleptjohn/phoenixvote/configuration/ConfigHandler.java
@@ -1,7 +1,6 @@
 package uk.co.ycleptjohn.phoenixvote.configuration;
 
 import java.io.File;
-import java.io.IOException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
@@ -20,44 +19,13 @@ public class ConfigHandler {
 	 * Saves config files - Or generates if non-existant
 	 */
 	public void saveConfigs() {
-		File datadir = PhoenixVote.getPlugin().getDataFolder();
-		YamlConfiguration general = new YamlConfiguration();
-		YamlConfiguration messages = new YamlConfiguration();
-		YamlConfiguration perks = new YamlConfiguration();
-		
-		try {
-			general.save(datadir.toString() + "general.yml");
-			messages.save(datadir.toString() + "messages.yml");
-			perks.save(datadir.toString() + "perks.yml");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for(Config c : configs.getConfigMap().values()) {
+			c.save();
 		}
-		
-		
-		if(!datadir.exists()) {
-			PhoenixVote.getPlugin().getDataFolder().mkdirs();
-		}
-		
-		
-		
-		
 	}
 	
 	public YamlConfiguration getConfig(String configToGet) {
-		File datadir = PhoenixVote.getPlugin().getDataFolder();
-		YamlConfiguration config = new YamlConfiguration();
-		
-		if (configToGet.equalsIgnoreCase("general")) {
-			config = YamlConfiguration.loadConfiguration(new File(datadir, "general.yml"));
-		}
-		else if (configToGet.equalsIgnoreCase("messages")) {
-			config = YamlConfiguration.loadConfiguration(new File(datadir, "messages.yml"));
-		}
-		else if (configToGet.equalsIgnoreCase("perks")) {
-			config = YamlConfiguration.loadConfiguration(new File(datadir, "perks.yml"));
-		}
-		return config;
+		return YamlConfiguration.loadConfiguration(new File(configs.getConfig(configToGet).getFilePath()));
 	}
 	
 	
@@ -70,4 +38,9 @@ public class ConfigHandler {
 		p.saveResource("general.yml", true);
 		
 	}
+	
+	//TODO Get-all methods
+	//TODO Get all config fields based on config name param
+	//TODO flesh out functional methods; save etc
+	
 }
