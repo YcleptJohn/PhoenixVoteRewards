929c6ddfacf93ab0330c9f22aafd1012c82f6967
John <YcleptJohn@gmail.com>
2016-11-25 23:35:44 +0000

    Converted test commands to use new Configs Enum

--- src/uk/co/ycleptjohn/voteshop/commands/cmdGENtest.java
+++ src/uk/co/ycleptjohn/voteshop/commands/cmdGENtest.java
@@ -3,11 +3,9 @@ package uk.co.ycleptjohn.voteshop.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.YamlConfiguration;
-
 import uk.co.ycleptjohn.voteshop.configuration.Config;
 import uk.co.ycleptjohn.voteshop.configuration.ConfigHandler;
+import uk.co.ycleptjohn.voteshop.configuration.Configs;
 
 public class cmdGENtest implements CommandExecutor {
 
@@ -17,8 +15,8 @@ public class cmdGENtest implements CommandExecutor {
 		//cf.regenerateAllConfigs();
 		ch.regenerateAllConfigsIfMissing();
 		
-		Config perks = ch.getConfig("perks");
-		
+		Config perks = ch.get(Configs.GENERAL);
+		perks.test();
 		
 		//yml.createSection("name.test.p");
 		
