5951e28b5f775a23211af83b01f5f8d2fdd9d1d8
John <YcleptJohn@gmail.com>
2016-11-25 23:56:35 +0000

    'Config'->'ConfigFile' && 'Configs Enum' -> 'Config Enum'
    The config class was renamed to ConfigFile and the Configs enum becamse
    Config enum instead for singularity and clarity. :+1:

--- src/uk/co/ycleptjohn/voteshop/configuration/Config.java
+++ src/uk/co/ycleptjohn/voteshop/configuration/Config.java
@@ -1,59 +1,30 @@
 package uk.co.ycleptjohn.voteshop.configuration;
 
 import java.io.File;
-import java.io.IOException;
 
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
 import uk.co.ycleptjohn.voteshop.VoteShop;
 
-public class Config extends YamlConfiguration {
-	private File file;
+public enum Config {
+	GENERAL("general", "general.yml"),
+	PERKS("perks", "perks.yml"),
+	MESSAGES("messages", "messages.yml");
+	
 	private Plugin plugin = VoteShop.getPlugin();
-	private String defaultConfigResourcePath;
+	private String name;
+	private ConfigFile conf;
 	
-	public Config(File configFile, String defaultConfigResourcePath) {
-		file = configFile;
-		try {
-			this.load(getFile());
-		} catch (IOException | InvalidConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		this.defaultConfigResourcePath = defaultConfigResourcePath;
+	Config(String configName, String fileName) {
+		name = configName;
+		conf = new ConfigFile(new File(plugin.getDataFolder(), fileName), fileName);
 	}
 	
-	public File getFile() {
-		return file;
+	public ConfigFile getConfigFile() {
+		return conf;
 	}
 	
-	
-	public void test() {
-		System.out.println(this.get("name"));
-	}
-	
-	public void reset() {
-		generateDefault();
-	}
-	
-	
-	public void generateDefaultIfMissing() {
-		boolean missing = true;
-		for(File f : plugin.getDataFolder().listFiles()) {
-			if(f.equals(this.getFile())) {
-				missing = false;
-			}
-		}
-		
-		if(missing) {
-			plugin.saveResource(defaultConfigResourcePath, false);
-		}
-	}
-	
-	public void generateDefault() {
-		plugin.saveResource(defaultConfigResourcePath, true);
+	public String getName() {
+		return name;
 	}
 }
---
+++ src/uk/co/ycleptjohn/voteshop/configuration/ConfigFile.java
@@ -0,0 +1,59 @@
+package uk.co.ycleptjohn.voteshop.configuration;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+import uk.co.ycleptjohn.voteshop.VoteShop;
+
+public class ConfigFile extends YamlConfiguration {
+	private File file;
+	private Plugin plugin = VoteShop.getPlugin();
+	private String defaultConfigResourcePath;
+	
+	public ConfigFile(File configFile, String defaultConfigResourcePath) {
+		file = configFile;
+		try {
+			this.load(getFile());
+		} catch (IOException | InvalidConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.defaultConfigResourcePath = defaultConfigResourcePath;
+	}
+	
+	public File getFile() {
+		return file;
+	}
+	
+
+	public void test() {
+		System.out.println(this.get("name"));
+	}
+	
+	public void reset() {
+		generateDefault();
+	}
+	
+	
+	public void generateDefaultIfMissing() {
+		boolean missing = true;
+		for(File f : plugin.getDataFolder().listFiles()) {
+			if(f.equals(this.getFile())) {
+				missing = false;
+			}
+		}
+		
+		if(missing) {
+			plugin.saveResource(defaultConfigResourcePath, false);
+		}
+	}
+	
+	public void generateDefault() {
+		plugin.saveResource(defaultConfigResourcePath, true);
+	}
+}
