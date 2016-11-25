bafe3f1b783a93c0418d5f3b1bf4eb4b2a16b7a1
John <YcleptJohn@gmail.com>
2016-11-25 23:56:52 +0000

    ConfigHandler converted to use Config Enum

--- src/uk/co/ycleptjohn/voteshop/configuration/ConfigHandler.java
+++ src/uk/co/ycleptjohn/voteshop/configuration/ConfigHandler.java
@@ -1,61 +1,35 @@
 package uk.co.ycleptjohn.voteshop.configuration;
 
 import java.io.File;
-import java.util.Map;
-
-import org.bukkit.plugin.Plugin;
-
-import uk.co.ycleptjohn.voteshop.VoteShop;
 
 public class ConfigHandler {
-	private ConfigList configs = new ConfigList();
-	private Plugin plugin = VoteShop.getPlugin();
+	//private Plugin plugin = VoteShop.getPlugin();
 	
 	public ConfigHandler() {
-		configs.add("general", new Config(new File(plugin.getDataFolder(), "general.yml"), "general.yml"));
-		configs.add("messages", new Config(new File(plugin.getDataFolder(), "messages.yml"), "messages.yml"));
-		configs.add("perks", new Config(new File(plugin.getDataFolder(), "perks.yml"), "perks.yml"));
 	}
 	
-	/**
-	 * Gets a config from the current list
-	 * @param configToGet config name to retrieve
-	 * @return Config obj of this config or null if it doesn't exist
-	 */
-	public Config get(Configs config) {
-		return configs.getConfig(config);
-		
+	public ConfigFile get(Config config) {
+		return config.getConfigFile();
 	}
 	
-	public File getFile(String configToGet) {
-		return configs.getConfig(configToGet).getFile();
+	public File getFile(Config config) {
+		return config.getConfigFile().getFile();
 	}
 	
-	public ConfigList getConfigList() {
-		return configs;
-	}
-	
-	public String getConfigListAsString() {
-		return configs.toString();
-	}
-	
-	public Map<String, Config> getConfigsMap() {
-		return configs.getConfigMap();
-	}
-	
-	public void regenerateConfig(String configToReset) {
-		configs.getConfig(configToReset).generateDefault();
+
+	public void regenerateConfig(Config configToReset) {
+		configToReset.getConfigFile().generateDefault();
 	}
 	
 	public void regenerateAllConfigs() {
-		for(Config c : configs.getConfigMap().values()) {
-			c.generateDefault();
+		for(Config c : Config.values()) {
+			c.getConfigFile().generateDefault();
 		}
 	}
 	
 	public void regenerateAllConfigsIfMissing() {
-		for(Config c : configs.getConfigMap().values()) {
-			c.generateDefaultIfMissing();
+		for(Config c : Config.values()) {
+			c.getConfigFile().generateDefault();
 		}
 	}
 	
